201c50fdb59f7519e6edcdbbcd9be6df1305fe5d
apavlov <AYPavlov@sberbank.ru>
2020-05-07 16:41:36 +0300

    Use pluses in sed

--- .github/scripts/decrypt_secring.sh
+++ .github/scripts/decrypt_secring.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output secring.pgp secring.gpg.gpg
+--output $HOME/secrets/secring.pgp secring.gpg.gpg
